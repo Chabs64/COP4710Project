2208ca1e988850dbd2dc2300c1bef87872649158
Chabs64 <chadhiggins6@gmail.com>
2023-12-06 13:33:09 -0500

    Minor Edits

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,15 @@ import { Link } from 'react-router-dom';
 export default function Home() {
     return(
         <>
-            <h2> Home Page </h2>
-            <h3> Directory </h3>
-            <p> For: PortEntry use <Link to="/PortEntry">PortEntry</Link> </p>
-            <p> For: PortExit use <Link to="/PortExit">PortExit</Link> </p>
-            <p> For: ShipRegistration use <Link to="/ShipRegistration">ShipRegistration</Link> </p>
-            <p> For: TruckRegistration use <Link to="/TruckRegistration">TruckRegistration</Link> </p>
-            <p> For: ContainerRegistration use <Link to="/ContainerRegistration">ContainerRegistration</Link> </p>
-            <p> For: TruckDriverRegistration use <Link to="/TruckDriverRegistration">TruckDriverRegistration</Link> </p>
-            <p> For: CraneOperator use <Link to="/CraneOperator">CraneOperator</Link> </p>
-            <p> For: PortAdmin use <Link to="/PortAdmin">PortAdmin</Link> </p>
+            <h2> Home Page Directory</h2>
+            <p> For: Port Entry use <Link to="/PortEntry">PortEntry</Link> </p>
+            <p> For: Port Exit use <Link to="/PortExit">PortExit</Link> </p>
+            <p> For: Ship Registration use <Link to="/ShipRegistration">ShipRegistration</Link> </p>
+            <p> For: Truck Registration use <Link to="/TruckRegistration">TruckRegistration</Link> </p>
+            <p> For: Container Registration use <Link to="/ContainerRegistration">ContainerRegistration</Link> </p>
+            <p> For: Truck Driver Registration use <Link to="/TruckDriverRegistration">TruckDriverRegistration</Link> </p>
+            <p> For: Crane Operator use <Link to="/CraneOperator">CraneOperator</Link> </p>
+            <p> For: Port Admin use <Link to="/PortAdmin">PortAdmin</Link> </p>
         </>
     )
 }
