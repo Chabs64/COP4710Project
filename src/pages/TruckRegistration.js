54a8edca0fcff7eab4ad94cb476a83fdf61c4e93
pranavjnair123 <nairpranavj@gmail.com>
2023-12-05 19:48:06 -0500

    registers now give message

--- src/pages/TruckRegistration.js
+++ src/pages/TruckRegistration.js
@@ -1,57 +1,70 @@
 import React, { useState } from 'react';
 
 export default function TruckRegistration() {
+  const [truckID, setTruckID] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
 
-    const [TruckID, setTruckID] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-      const handleSubmit = async (e) => {
-        e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:4000/api/addtruck', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          trucksID: truckID,
+        }),
+      });
 
-        try {
-          const response = await fetch('http://localhost:4000/api/addtruck', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              trucksID: TruckID
-            }),
-          });
+      if (!response.ok) {
+        throw new Error('Error registering truck');
+      }
 
-          if (!response.ok) {
-            throw new Error('Error registering truck');
-          }
+      // Reset error message on success
+      setErrorMessage('');
 
-          // Handle success if needed
-        } catch (error) {
-          // Handle error
-          console.error('Error:', error.message);
-        }
-      };
+      // Show success message
+      setSuccessMessage('Truck Registered!');
 
-    return(
-        <>
-            <>
-              <h2>Truck Registration</h2>
-              <form onSubmit={handleSubmit}>
-                <label htmlFor="TruckID">Truck Registration:</label>
-                <br />
-                <input
-                  type="text"
-                  id="TruckID"
-                  name="TruckID"
-                  value={TruckID}
-                  onChange={(e) => setTruckID(e.target.value)}
-                  placeholder="Enter Truck ID Here"
-                />
-                <br />
-                <input type="submit" defaultValue="Submit" />
-              </form>
-              <p>
-                Enter the truck ID of the truck. It will display an error if it
-                already exists.
-              </p>
-            </>
-        </>
-    )
+      // Hide success message after 3 seconds
+      setTimeout(() => {
+        setSuccessMessage('');
+      }, 5000);
+
+      // Reset truckID after successful registration (optional)
+      setTruckID('');
+    } catch (error) {
+      // Handle error
+      console.error('Error:', error.message);
+      setErrorMessage(error.message);
+    }
+  };
+
+  return (
+    <>
+      <h2>Truck Registration</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="truckID">Truck Registration:</label>
+        <br />
+        <input
+          type="text"
+          id="truckID"
+          name="truckID"
+          value={truckID}
+          onChange={(e) => setTruckID(e.target.value)}
+          placeholder="Enter Truck ID Here"
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+      <p>
+        Enter the truck ID of the truck. It will display an error if it already exists.
+      </p>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
+    </>
+  );
 }
